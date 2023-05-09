81b367a7015639f6b4ba87183f0854cc5b975e2e
christian <christiancoronel1986@gmail.com>
2023-05-09 11:11:02 -0600

    resuelto

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,8 +1,25 @@
 'use strict';
 
-function BinarioADecimal(num) {}
+function BinarioADecimal(num) {
+   let decimal = 0;
+   let exponente = 0;
+   for(let i = num.length - 1; i >=0; i--){
+      let digito = parseInt(num[i]);
+      decimal += digito * Math.pow(2, exponente);
+      exponente++
+   }
+   return decimal;
+}
 
-function DecimalABinario(num) {}
+function DecimalABinario(num) {
+   let numBinario = "";
+   while(num > 0){
+      let residuo = num % 2;
+      numBinario = residuo + numBinario;
+      num = Math.floor(num / 2);
+   }
+   return numBinario;
+}
 
 module.exports = {
    BinarioADecimal,
