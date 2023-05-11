3c60355a806fa51ea30b24f47dead539d6bbc714
christian <christiancoronel1986@gmail.com>
2023-05-11 10:47:07 -0600

    resuelto

--- 03-JavaScriptAvanzado-II/homework/homework.js
+++ 03-JavaScriptAvanzado-II/homework/homework.js
@@ -6,15 +6,23 @@
 La función counter debe retornar otra función. Esta función retornada debe actuar como un contador, retornando 
 un valor numérico que empieza en 1 e incrementa con cada invocación.
 EJEMPLO
-const nuevoContador = counter()
+const nuevoContador = counter()ö
 nuevoContador()     // 1
 nuevoContador()     // 2
 
 const otroContador = counter()
 otroContador()      // 1
 otroContador()      // 2 */
-function counter() {}
-
+function counter() {
+  let cont = 0;
+  return function(){
+    cont++;
+    return cont;
+  }
+}
+const nuevoContador = counter();
+const otroContador = counter()
+console.log(nuevoContador());
 /* Ejercicio 2
 Tu tarea aquí es lograr, mediante un closure, que cacheFunction actúe como una memoria caché para el callback 
 que recibe por parámetro (cb); es decir, que "recuerde" el resultado de cada operación que hace, de manera que, 
@@ -32,9 +40,26 @@ otra vez cálculos que ya se hicieron anteriormente.
 
   squareCache(5)    // invocará a square(5), almacenará el resultado y lo retornará
   squareCache(5)    // no volverá a invocar a square, simplemente buscará en la caché cuál es el resultado de square(5) y lo retornará (tip: si usaste un objeto, podés usar hasOwnProperty) */
+function cacheFunction(cb) {
+    const cache = {}
+    return function(arg){
+      if (cache.hasOwnProperty(arg)) {
+        return cache[arg];
+    }else{
+      const result = cb(arg);
+      cache[arg] = result;
+      return result;
+    }
+  }
+}
+function square(n){
+  return n * n
+}
 
-function cacheFunction(cb) {}
-
+const squareCache = cacheFunction(square)
+console.log(squareCache(5));
+console.log(squareCache(7));
+console.log(squareCache(7));
 //----------------------------------------
 
 // Bind
@@ -58,8 +83,8 @@ function getNombre() {
   Usando el método bind() guardar, en las dos variables declaradas a continuación, dos funciones que actúen como getNombre pero retornen el nombre del instructor y del alumno, respectivamente.
 */
 
-let getNombreInstructor = getNombre.bind();
-let getNombreAlumno = getNombre.bind();
+let getNombreInstructor = getNombre.bind(instructor);
+let getNombreAlumno = getNombre.bind(alumno);
 
 /*
   Ejercicio 4
@@ -70,9 +95,9 @@ function crearCadena(delimitadorIzquierda, delimitadorDerecha, cadena) {
     return delimitadorIzquierda + cadena + delimitadorDerecha;
 }
 
-let textoAsteriscos = crearCadena.bind();
-let textoGuiones = crearCadena.bind();
-let textoUnderscore = crearCadena.bind();
+let textoAsteriscos = crearCadena.bind(null,"*","*");
+let textoGuiones = crearCadena.bind(null,"-","-");
+let textoUnderscore = crearCadena.bind(null,"_","_");
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
